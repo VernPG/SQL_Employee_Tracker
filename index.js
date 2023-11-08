71c60df8cdb52676f173890b9fe713d3f8605c7d
Veronica Perez <veperez@live.com>
2023-11-08 17:18:43 -0600

    fixed add role function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // const express = require('express');
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
 // Import and require mysql2
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 // const PORT = process.env.PORT || 3001;
 // const app = express();
@@ -13,205 +13,280 @@ const mysql = require('mysql2');
 // Connect to database
 const connection = mysql.createConnection(
   {
-    host: 'localhost',
+    host: "localhost",
     // MySQL username,
-    user: 'root',
+    user: "root",
     // TODO: Add MySQL password here
-    password: 'Molly2112!',
-    database: 'tracker_db'
+    password: "Molly2112!",
+    database: "tracker_db",
   },
   console.log(`Connected to the tracker_db database.`)
 );
-// connection.connect(err => {
-//   if (err) throw err;
-//   console.log('connected as id ' + connection.threadId);
-//   afterConnection();
-// });
-// afterConnection = () => {
-//   console.log("***********************************")
-//   console.log("*                                 *")
-//   console.log("*        EMPLOYEE MANAGER         *")
-//   console.log("*                                 *")
-//   console.log("***********************************")
-//   init();
-// };
+connection.connect((err) => {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  afterConnection();
+});
+afterConnection = () => {
+  console.log("*****************************");
+  console.log("*                           *");
+  console.log("*    EMPLOYEE TRACKER       *");
+  console.log("*                           *");
+  console.log("*****************************");
+  init();
+};
 
-const init = () =>{
-    inquirer.prompt([{
-      name:"choices",
-      type:"list",
-      message:"What would you like to do?",
-      choices:[
-        "view all departments", 
-        "view all roles", 
-        "view all employees", 
-        "add a department", 
-        "add a role", 
-        "add an employee", 
-        "update an employee role"
-      ]
-    }])
-// add all question repsonses
-    .then ((answers) => {
-      const {choices} = answers;
-      console.log(choices, answers)
-       if (choices === "view all departments"){
+const init = () => {
+  inquirer
+    .prompt([
+      {
+        name: "choices",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "view all departments",
+          "view all roles",
+          "view all employees",
+          "add a department",
+          "add a role",
+          "add an employee",
+          "update an employee role",
+        ],
+      },
+    ])
+    // add all question repsonses
+    .then((answers) => {
+      const { choices } = answers;
+      console.log(choices, answers);
+      if (choices === "view all departments") {
         showDepartments();
-       }
-       if (choices === "view all roles"){
+      }
+      if (choices === "view all roles") {
         showRoles();
-       }
-       if (choices === "view all employees"){
-        showEmployees();        
-       }
-       if (choices === "add a department"){
+      }
+      if (choices === "view all employees") {
+        showEmployees();
+      }
+      if (choices === "add a department") {
         addDepartment();
-       }
-       if (choices === "add a role"){
-        addRole();        
-       }
-       if (choices === "add an employee"){
-        addEmployee();        
-       }
-       if (choices === "update an employee role"){
+      }
+      if (choices === "add a role") {
+        addRole();
+      }
+      if (choices === "add an employee") {
+        addEmployee();
+      }
+      if (choices === "update an employee role") {
         updateEmployee();
-       };
+      }
+    });
+};
+//works
+showDepartments = () => {
+  console.log;
+  ("Show departments");
+  const sql = `SELECT * FROM department`;
+
+  connection
+    .promise()
+    .query(sql)
+    .then((data) => {
+      console.table(data[0]);
+      init();
     })
-    
+    .catch((err) => console.log(err));
 };
-showDepartments = () =>{
-  console.log;('Show departments');
-  const sql = `SELECT * FROM department`; 
+//works
+showRoles = () => {
+  console.log;
+  ("Show role");
+  const sql = `SELECT * FROM role`;
 
-  connection.promise().query(sql)
-  .then(data => {
-    console.table(data [0]);
-    init();
-  })
-  .catch(err => 
-    console.log(err));
-}
-
-showRoles= () =>{
-  console.log;('Show role');
-  const sql = `SELECT * FROM role`; 
-
-  connection.promise().query(sql)
-  .then(data => {
-    console.table(data [0]);
-    init();
-  })
-  .catch(err => 
-    console.log(err));
-}
-
-
-
-showEmployees= () =>{
-  console.log;('Show employee');
-  const sql = `SELECT * FROM employee`; 
-
-  connection.promise().query(sql)
-  .then(data => {
-    console.table(data [0]);
-    init();
-  })
-  .catch(err => 
-    console.log(err));
-}
-     
-
-
- addDepartment= () =>{
-  const sql = `INSERT INTO department (name) VALUES (?)`
-  inquirer.prompt([{
-    name:"deptName",
-    type:"input",
-    message:"Please type in a department would you like to add?",
- 
-   }])
-   .then(answers => {
-    console.log(answers)
-  
- 
-  connection.promise().query(sql, answers.deptName)
-  .then(data => {
-    console.table(data [0]);
-    init();
-  })
-  .catch(err => 
-    console.log(err));
-  }) 
+  connection
+    .promise()
+    .query(sql)
+    .then((data) => {
+      console.table(data[0]);
+      init();
+    })
+    .catch((err) => console.log(err));
 };
+//works
+showEmployees = () => {
+  console.log;
+  ("Show employee");
+  const sql = `SELECT * FROM employee`;
 
+  connection
+    .promise()
+    .query(sql)
+    .then((data) => {
+      console.table(data[0]);
+      init();
+    })
+    .catch((err) => console.log(err));
+};
+//works
+addDepartment = () => {
+  const sql = `INSERT INTO department (name) VALUES (?)`;
+  inquirer
+    .prompt([
+      {
+        name: "deptName",
+        type: "input",
+        message: "Please type in a department would you like to add?",
+      },
+    ])
+    .then((answers) => {
+      console.log(answers);
 
-
- addRole= () =>{
-  const sql = `INSERT INTO role (name) VALUES (?, ?, ?)`
-  inquirer.prompt([{
-    name:"deptName",
-    type:"input",
-    message:"Please type in a department would you like to add?",
- 
-   }])
-
-
-  
- 
-  connection.promise().query(sql, answers.roleName)
-  .then(data => {
-    console.table(data [0]);
-    init();
-  })
-  .catch(err => 
-    console.log(err));
+      connection
+        .promise()
+        .query(sql, answers.deptName)
+        .then((data) => {
+          console.table(data[0]);
+          init();
+        })
+        .catch((err) => console.log(err));
+    });
+};
+//works
+addRole = async () => {
+  const sql = `INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`;
+  const departmentsData = await connection
+    .promise()
+    .query("Select * from department");
+  console.log(departmentsData);
+  let formattedData = [];
+  for (let i = 0; i < departmentsData[0].length; i++) {
+    formattedData.push({
+      name: departmentsData[0][i].name,
+      value: departmentsData[0][i].id,
+    });
   }
-    
+  console.log(formattedData);
+  inquirer
+    .prompt([
+      {
+        name: "department_id",
+        type: "list",
+        message:
+          "Please select a department you would like to add the new role?",
+        choices: formattedData,
+      },
+      {
+        name: "title",
+        type: "input",
+        message: "What is the title name for the new role?",
+      },
+      {
+        name: "salary",
+        type: "input",
+        message: "What is the salary for the new role?",
+      },
+    ])
+    .then((answers) => {
+      console.log(answers);
 
-//  addEmployee= () =>{
-//   const sql = `INSERT INTO employee (name) VALUES (?)`
-//   inquirer.prompt([{
-//     name:"empName",
-//     type:"input",
-//     message:"Please type in the employee name you would you like to add?",
- 
-//    }])
-//    .then(answers => {
-//     console.log(answers)
-  
- 
-//   connection.promise().query(sql, [answers.empName, ])
-//   .then(data => {
-//     console.table(data [0]);
-//     init();
-//   })
-//   .catch(err => 
-//     console.log(err));
-//   }) 
-// }
-      
+      connection
+        .promise()
+        .query(sql, [answers.title, answers.salary, answers.department_id])
+        .then((data) => {
+          console.table(data[0]);
+          init();
+        })
+        .catch((err) => console.log(err));
+    });
+};
 
-//  updateEmployee= () =>{
-//   console.log;
-//   connection.promise().query(sql, (err, rows) => {
-//     if (err) throw err;
-//     console.table(rows);
-//     promptUser();
-//   });
-// }
+addEmployee = async () => {
+  const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?)`;
+  const departmentsData = await connection
+    .promise()
+    .query("Select * from role");
+  console.log(departmentsData);
+  let formattedData = [];
+  for (let i = 0; i < departmentsData[0].length; i++) {
+    formattedData.push({
+      name: departmentsData[0][i].name,
+      value: departmentsData[0][i].id,
+    });
+  }
+  console.log(formattedData);
+  inquirer
+    .prompt([
+      {
+        name: "role_id",
+        type: "list",
+        message:
+          "Please select a role you would like to assign to the new employee?",
+        choices: formattedData,
+      },
+      {
+        name: "title",
+        type: "input",
+        message: "What is the first name of the new employee?",
+      },
+      {
+        name: "title",
+        type: "input",
+        message: "What is the last name of the new employee?",
+      },
+    ])
+    .then((answers) => {
+      console.log(answers);
 
+      connection
+        .promise()
+        .query(sql, [answers.first_name, answers.last_name, answers.role_id])
+        .then((data) => {
+          console.table(data[0]);
+          init();
+        })
+        .catch((err) => console.log(err));
+    });
+};
 
+updateEmployee = async () => {
+  const sql = `UPDATE employee SET (first_name, last_name, role_id, manager_id) WHERE id = (?)`;
+  const departmentsData = await connection
+    .promise()
+    .query("Select * from role");
+  console.log(departmentsData);
+  let formattedData = [];
+  for (let i = 0; i < departmentsData[0].length; i++) {
+    formattedData.push({
+      name: departmentsData[0][i].name,
+      value: departmentsData[0][i].id,
+    });
+  }
+  console.log(formattedData);
+  inquirer
+    .prompt([
+      {
+        name: "updateEmp",
+        type: "list",
+        message: "Please select a employee you would like to add the new role?",
+        choices: formattedData,
+      },
+      {
+        name: "title",
+        type: "input",
+        message: "What is the title name for the new role?",
+      },
+    ])
+    .then((answers) => {
+      console.log(answers);
+
+      connection
+        .promise()
+        .query(sql, [answers.first_name, answers.last_name, answers.updateEmp])
+        .then((data) => {
+          console.table(data[0]);
+          init();
+        })
+        .catch((err) => console.log(err));
+    });
+};
 
 init();
-
-
-// "select * from departemnt"
-
-// loop over that data and create an array with objects containing a name(to show to users) and a value(to be selected under the hood) property
-// var someArr = [{name:"HR", value: 1}]
-// inquirer.prompt([{
-//   name:"choices",
-//   type:"list",
-//   message:"What would you like to do?",
-//   choices:someArr
-// }])
